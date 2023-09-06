0b1c8007f59196577c47da8d674cd0de6f1d559a
pokycookie <0cookieboy0@gmail.com>
2023-09-06 18:44:19 +0900

    feat: calendar, dayjs, fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Calendar from './components/calendar/calendar'
+
 function App() {
   return (
     <>
-      <main className="w-vw h-screen"></main>
+      <main className="h-screen w-vw">
+        <div className="w-96 h-96">
+          <Calendar />
+        </div>
+      </main>
     </>
   )
 }
